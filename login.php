9db0519f41265e89a70a29a1b9fbf89f8953288a
dw1991 <d.watson.1991@gmail.com>
2011-01-27 19:20:39 +0000

    updated login.php so that it now connect to the database and will only allow access with a correct username and password

--- login.php
+++ login.php
@@ -5,21 +5,31 @@
 $username="js230";
 $password="js230";
 $database="js230";
-$host = "www2";
+$host = "anubis.macs.hw.ac.uk";
 
 $user = $_POST['username'];
-$pass = md5($_POST['password']);
-
+$pass = $_POST['password'];
 
 $link = mysql_connect($host,$username,$password) or die('Could not connect: ' . mysql_error());
-@mysql_select_db($database) or die( "Unable to select database");
 
-$query = "SELECT FROM staff WHERE staff_ID='username' AND password ='password'";
-$result = mysql_query($query);
+mysql_select_db($database) or die( "Unable to select the database: " . $database . " ");
 
-echo "Hello ". $user . " " . $pass . ".<br />";
-echo "Thank you for logging in!";
+$sql = "SELECT * FROM staff WHERE user_name = '$user' AND password = '$pass'";
 
+$result = mysql_query($sql);
+
+// Mysql_num_row is counting table row
+$count = mysql_num_rows($result);
+
+// If result matched $myusername and $mypassword, table row must be 1 row
+if($count == 1){
+	// Register $myusername, $mypassword and redirect to file "admin-access.php"
+	session_register("user");
+	session_register("pass");
+	header("location:admin-access.php");
+}
+//displays error message otherwise
+else {
+	echo "Wrong Username or Password";
+}
 ?>
-
-</body></html>
