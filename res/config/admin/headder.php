7d42fd7dd5428249debc9476d50e716ad8e278ab
illumi <jamiespence2@gmail.com>
2011-02-02 21:27:53 +0000

    Tweaking the admin menus, not finished by any means :)

--- res/config/admin/headder.php
+++ res/config/admin/headder.php
@@ -25,13 +25,20 @@ Computer Science and Information Systems Year 3
 		<hgroup>
 			<h1>Jworld</h1>
 		</hgroup>
+		
+		
 	<nav>
-		<center><ul>
-			<li><a href="admin.php">Home</a></li>
-			<li><a href="admin.php?page=staff-acs">Staff</a></li>
-			<li><a href="admin.php?page=promo-acs">Promotions</a></li>
-			<li><a href="admin.php?page=tt-acs">Timetable</a></li>
-			<li><a href="admin.php?page=stat-acs">Statistics</a></li>
-		</ul></center>
+		<li><a href="admin.php">Home</a></li>
+		<li><a href="admin.php?page=staff-acs">Staff</a>
+			<ul>
+				<li><a href="admin.php?page=add-staff">Add Staff</a></li>
+				<li><a href="admin.php?page=edit-staff">Edit Staff</a></li>
+				<li><a href="admin.php?page=remove-staff">Remove Staff</a></li>
+			</ul>
+		</li>
+
+		<li><a href="admin.php?page=promo-acs">Promotions</a></li>
+		<li><a href="admin.php?page=tt-acs">Timetable</a></li>
+		<li><a href="admin.php?page=stat-acs">Statistics</a></li>
 	</nav>
 </header>
