6e5a50d641fed8ea746691df9d79b7e6b885b004
nb135 <breminem@hotmail.co.uk>
2011-02-07 18:04:35 +0000

    added dropdown box to add-promo

--- res/pages/admin/add-promo.php
+++ res/pages/admin/add-promo.php
@@ -4,6 +4,26 @@
 {
 	header('Location: ../../../index.php?page=adminLogin');
 }
+
+include 'sql-connection.php';
+
+$con=mysql_connect($host,$username,$password) or die(mysql_error());
+
+mysql_select_db($database) or die(mysql_error());
+
+$filmQuery="SELECT film_ID,film_title FROM films";
+
+$filmResult = mysql_query($filmQuery);
+
+$filmOptions="";
+
+while ($row=mysql_fetch_array($filmResult)){
+$filmid=$row['film_ID'];
+$filmname=$row['film_title'];
+$filmOptions.="<OPTION VALUE=\"$filmid\">".$filmname."</option>";
+}
+
+
 ?>
 
 <div id="body">
@@ -11,15 +31,24 @@
 <center>
 <p>
 <div id="header">
-	<input type=button onClick="location.href='admin.php?page=promo-acs'" value='Promotion Overview'>
-	<input type=button onClick="location.href='admin.php?page=edit-promo'" value='Edit Promotion'>
-	<input type=button onClick="location.href='admin.php?page=remove-promo'" value='Remove Promotion'>
+	<input type=button onClick="location.href='admin.php?page=promo-acs'" value='Promotions
+Overview'>
+	<input type=button onClick="location.href='admin.php?page=add-promo'" value='Add
+Promotion'>
+	<input type=button onClick="location.href='admin.php?page=edit-promo'" value='Edit
+Promotion'>
+	<input type=button onClick="location.href='admin.php?page=remove-promo'" value='Remove
+Promotion'>
 
 </div>
+<h2>
 <section id="new promotion info">
 	<form method="POST" action="res/pages/admin/add-pro.php">
-	Film ID: <input type="text" value="" id="filmid" required> <br />
-	Name: <input type="text" value="" id="promoname" required> <br />
+	Film: <select NAME="filmid" required>
+		<option VALUE="0">Film Title
+		<? echo $filmOptions?>
+		</select> <br />
+	Promotion Name: <input type="text" value="" id="promoname" required> <br />
 	Start Date: (Format: YYYY-MM-DD) <input type="text" value="" id="start" required> <br />
 	End Date: (Format: YYYY-MM-DD) <input type="text" value="" id="end" required> <br />
 	Description: <input type="text" value="" id="desc" required> <br />
@@ -27,5 +56,9 @@
 		<input type="reset" name="reset" id="reset" value="Reset"> <br />
 	</form>
 	</section>
+</h2>
 </center>
 </div>
+<?php
+mysql_close($con);
+?>
