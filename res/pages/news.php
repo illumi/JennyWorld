9721e4d5775ec84d8131654c79369cbb340e701f
dw1991 <d.watson.1991@gmail.com>
2011-03-24 22:25:01 +0000

    imlemented changes to events: now oly shows promotions to customer with end date >= today

--- res/pages/news.php
+++ res/pages/news.php
@@ -7,8 +7,9 @@ Software Engineering Group Project
 Computer Science and Information Systems Year 3
 
 **********************************************/ 
-
+$today = date('Y-m-d');
 ?>
+
 <div id="body">
 <h1>Current promotions</h1>
 <h3>
@@ -19,25 +20,25 @@ Computer Science and Information Systems Year 3
 	include 'sql-connection.php';
 
 
-	$query = "SELECT f.film_title, p.promo_name, p.end_date, p.description FROM films f, promotions p WHERE f.film_ID = p.film_ID;";
+	$query = " SELECT promo_name, film_title, description, end_date FROM promotions p, films f WHERE p.film_ID = f.film_ID AND p.end_date >= $today;";
 		 
 	$result = mysql_query($query) or die(mysql_error());
 
 	echo "<table class=\"center\">
 	<tr>
+	<th>Promotion</th>
 	<th>Film Title</th>
-	<th>Name</th>
-	<th>End Date</th>
 	<th>Description</th>
+	<th>End Date</th>
 	</tr>";
 	// Print out the contents of each row into a table 
 	while($row = mysql_fetch_array($result)){
 
 		echo "<tr>";
-		echo "<td>" . $row['film_title'] . "</td>";
 		echo "<td>" . $row['promo_name'] . "</td>";
-		echo "<td>" . $row['end_date'] . "</td>";
+		echo "<td>" . $row['film_title'] . "</td>";
 		echo "<td>" . $row['description'] . "</td>";
+		echo "<td>" . $row['end_date'] . "</td>";
 		echo "</tr>";
 	}
 	echo "</table>";
