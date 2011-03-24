a0feb1861eb3ec21e956153359f3cd6eb0a656d3
illumi <jamiespence2@gmail.com>
2011-03-24 12:13:39 +0000

    fixed positioning of booking box and starting adding more statistics

--- res/pages/admin/stat-acs.php
+++ res/pages/admin/stat-acs.php
@@ -4,16 +4,16 @@
 	header('Location: ./index.php?page=adminLogin');
 }
 
-include 'sql-connection.php';
+include('res/lib/class_dbcon.php');
+$connect = new doConnect();
 
 $query = mysql_query("SELECT SUM(numof_tickets) FROM booking");
+$total = mysql_fetch_assoc($query);
 
-$total = mysql_fetch_assoc($query)
+$query = mysql_query("SELECT showing_id, sum(numof_tickets) AS total FROM booking GROUP BY showing_id ORDER BY total DESC"); //for showing ID, can be done on whole films.
+$mostPopular = mysql_fetch_assoc($query);
 
-//$query = mysql_query("SELECT showing_id, sum(numof_tickets) AS total FROM booking GROUP BY showing_id ORDER BY total DESC"); //for showing ID, can be done on whole films.
-
-//$mostPopular = mysql_fetch_assoc($query)
-
+$connect->disc();
 ?>
 
 <div id="body">
@@ -22,6 +22,8 @@ $total = mysql_fetch_assoc($query)
 
 <?php
 	echo "Total number of customers: " . $total['SUM(numof_tickets)'];
+	
+	echo "<p> The most popular film to date is showing: " . $mostPopular['showing_id'] . " with number of viewers: ". $mostPopular['total'];
 ?>
 
 </h2>
