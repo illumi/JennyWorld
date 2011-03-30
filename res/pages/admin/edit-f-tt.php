edef20fa1350f1f2a8f5990fe50c669b4f15932a
Kyle Ewan Alexander Tough <keat1@linux79.(none)>
2011-03-30 15:02:43 +0100

    edit tt wont work, i added js submit option but still wont work

--- res/pages/admin/edit-f-tt.php
+++ res/pages/admin/edit-f-tt.php
@@ -9,22 +9,26 @@ include('res/lib/class_dbcon.php');
 $connect = new doConnect();
 
 	//New fields to change
-	$showingID = $_POST['newShowingID'];
+	$showingID = $_POST['ShowingID'];
 	$screen = $_POST['newScreen'];
+///$screen = $_POST['newFilm'];
 	$start_date = $_POST['start'];
 	$end_date =  $_POST['end'];
 	$start_time = $_POST['newTime'];
 
-	mysql_query("UPDATE showings SET showing_ID = '$showingID', screen_ID = '$screen', start_time='$start_time', start_date = '$start_date', end_date = '$end_date' WHERE showing_ID = '$showingID'") or die(mysql_error()) ;
+$sql="UPDATE showings SET screen_ID = '$screen', start_date = '$start_date', end_date = '$end_date', start_time='$start_time' WHERE showing_ID = '$showingID'");
 
+
+mysql_query($sql) or die(mysql_error()) ;
+/**
 	$check = mysql_query("SELECT * FROM showings") or die(mysql_error());
 
 	while($row = mysql_fetch_array($check)){
 		echo " Stime -".$row['start_time']. " - Screen -". $row['screen_ID']. " - ShowingID -". $row['showing_ID'];
 		echo "<br />";
 	}
-
+*/
 $connect->disc();
-header("location: admin.php?page=remove-film-tt");
+header("location: admin.php?page=tt-acs");
 
 ?>
