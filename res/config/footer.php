cfabcaf0cd5d530b80158e27d7f95646a8e73f11
dw1991 <d.watson.1991@gmail.com>
2011-04-01 01:06:27 +0100

    added style to links

--- res/config/footer.php
+++ res/config/footer.php
@@ -15,16 +15,16 @@ Computer Science and Information Systems Year 3
 				<?php
 				if(isset($_SESSION['admin'])) {
 					if($_SESSION['admin'] == 1  && $_SESSION['staff'] == 1){
-						echo "<a href=\"admin.php?page=switchStaffView\">Manager View</a><p></h2>";
+						echo "<div class=\"lb\"><a href=\"admin.php?page=switchStaffView\">Manager View</a><p></h2></div>";
 					} else if ($_SESSION['admin'] == 1) {
-						echo "<a href=\"admin.php?page=switchStaffView\">Staff view</a><p></h2>";
+						echo "<div class=\"lb\"><a href=\"admin.php?page=switchStaffView\">Staff view</a><p></h2></div>";
 					}
 				}
 				if(isset($_SESSION['login'])) {
-					echo "<a href=\"admin.php?page=logOut\">Log Out</a>";
+					echo "<div class=\"lb\"><a href=\"admin.php?page=logOut\">Log Out</a></div>";
 				}
 				else {
-					echo "<a href=\"index.php?page=adminLogin&fail=\">Staff Login</a>";
+					echo "<div class=\"lb\"><a href=\"index.php?page=adminLogin&fail=\">Staff Login</a></div>";
 				}
 				?>
 				<br/>
